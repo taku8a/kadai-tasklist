31506cb2e378bc13e7267595445b38203c363149
taku8a <takuya0505hsmt@gmail.com>
2021-09-30 09:03:26 +0000

    test

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,15 @@
 class TasksController < ApplicationController
   before_action :set_task, only:[:show,:edit,:update,:destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+  
 
   def index
-      @tasks=Task.all
+    if logged_in?
+      @task = current_user.tasks.build
+      @pagy,@tasks = pagy(current_user.tasks.order(id: :desc))
+    end
+    
   end
 
   def show
@@ -13,11 +20,12 @@ class TasksController < ApplicationController
   end
 
   def create
-      @task=Task.new(task_params)
+      @task=current_user.tasks.build(task_params)
       if @task.save
          flash[:success]="Task投稿完了"
-         redirect_to task_path(@task)
+         redirect_to root_url
       else
+          @pagy,@tasks = pagy(current_user.tasks.order(id: :desc))
           flash.now[:danger]="Taskが投稿されませんでした"
           render :new
           
@@ -43,19 +51,26 @@ class TasksController < ApplicationController
       @task.destroy
       
       flash[:success]="Task削除完了"
-      redirect_to tasks_path
+      redirect_to root_path
   end
   
   private
   
   def set_task
-      @task=Task.find(params[:id])
+      @task=Task.find_by(id: params[:id])
      
   end
   
   
   def task_params
       params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+      @task = current_user.tasks.find_by(id: params[:id])
+      unless @task
+          redirect_to root_url
+      end
   end
   
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
     root to: "tasks#index"
     resources :tasks
+    get "login", to: "sessions#new"
+    post "login", to: "sessions#create"
+    get "logout", to: "sessions#destroy"
+    
+    get "signup",to: "users#new"
+    resources :users, only: [:index, :show, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
